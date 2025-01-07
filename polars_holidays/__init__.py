d288dbebfe0eb5907743d73629decd19a8fe83ee
jsj <jsj>
2025-01-07 15:49:28 +0100

    expose plugins in python

--- polars_holidays/__init__.py
+++ polars_holidays/__init__.py
@@ -14,52 +14,19 @@ if TYPE_CHECKING:
 LIB = Path(__file__).parent
 
 
-# def pig_latinnify(expr: IntoExprColumn) -> pl.Expr:
-#     return register_plugin_function(
-#         args=[expr],
-#         plugin_path=LIB,
-#         function_name="pig_latinnify",
-#         is_elementwise=True,
-#     )
-
-# def noop(expr: IntoExprColumn) -> pl.Expr:
-#     return register_plugin_function(
-#         args=[expr],
-#         plugin_path=LIB,
-#         function_name="noop",
-#         is_elementwise=True,
-#     )
-
-# def abs_i64(expr: IntoExprColumn) -> pl.Expr:
-#     return register_plugin_function(
-#         args=[expr],
-#         plugin_path=LIB,
-#         function_name="abs_i64",
-#         is_elementwise=True,
-#     )
-
-# def abs_numeric(expr: IntoExprColumn) -> pl.Expr:
-#     return register_plugin_function(
-#         args=[expr],
-#         plugin_path=LIB,
-#         function_name="abs_numeric",
-#         is_elementwise=True,
-#     )
-
-def is_holiday(expr: IntoExprColumn, *, country_code: str) -> pl.Expr:
+def is_holiday(date: IntoExprColumn, country_code: IntoExprColumn) -> pl.Expr:
     return register_plugin_function(
-        args=[expr],
+        args=[date, country_code],
         plugin_path=LIB,
         function_name="is_holiday",
         is_elementwise=True,
-        kwargs={"country_code": country_code},
     )
 
-def get_holiday(expr: IntoExprColumn, *, country_code: str) -> pl.Expr:
+
+def get_holiday(date: IntoExprColumn, country_code: IntoExprColumn) -> pl.Expr:
     return register_plugin_function(
-        args=[expr],
+        args=[date, country_code],
         plugin_path=LIB,
         function_name="get_holiday",
         is_elementwise=True,
-        kwargs={"country_code": country_code},
     )
